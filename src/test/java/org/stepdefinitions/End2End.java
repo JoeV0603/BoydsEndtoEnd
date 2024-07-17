bc5bf658db2e21e17ad691f3c6c584720a868f9a
JoeV0603 <vishal@hurekatek.com>
2024-07-17 11:02:31 +0530

    Update End2End.java

--- src/test/java/org/stepdefinitions/End2End.java
+++ src/test/java/org/stepdefinitions/End2End.java
@@ -1,5 +1,7 @@
 package org.stepdefinitions;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import java.time.Duration;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,17 +23,17 @@ public class End2End {
 
 		driver.manage().window().maximize();
 
-	Thread.sleep(1000);
+		Thread.sleep(1000);
 		
 		driver.findElement(By.xpath("(//a[@class=\'main-login'])")).click();
 			
-	Thread.sleep(1000);
+		Thread.sleep(1000);
 		
 		driver.findElement(By.name("username")).sendKeys("vishal@hurekatek.com");
 		
-		driver.findElement(By.name("password")).sendKeys("Joevishal@0603");
+		driver.findElement(By.name("password")).sendKeys("Joevishal@6394");
 		
-	Thread.sleep(1000);
+		Thread.sleep(1000);
 		
 		driver.findElement(By.xpath("//button[@class='woocommerce-button button woocommerce-form-login__submit']")).click();
 
@@ -41,36 +43,40 @@ public class End2End {
     
 	public void User_navigates_to_shop_all_tab() throws InterruptedException {
 
-        Thread.sleep(1000);
+	    
+		Thread.sleep(1000);
 		
 		driver.findElement(By.xpath("(//a[@href='https://boyds.wp.shottqsr.com/shop/']) [1]")).click();
 		
-	Thread.sleep(1000);
+		Thread.sleep(1000);
 		
 		driver.findElement(By.id("product-search")).sendKeys("Pomegranate Herbal Tea");
 		
-	Thread.sleep(5000);
+		Thread.sleep(5000);
 		
-		driver.findElement(By.xpath("//a[@href='https://boyds.wp.shottqsr.com/product/farmer-brothers-premium-pomegranate-herbal-tea/']")).click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		
-	Thread.sleep(2000);
+		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='https://boyds.wp.shottqsr.com/product/farmer-brothers-premium-pomegranate-herbal-tea/']")));
+		element.click();
+
+		Thread.sleep(2000);
 		
 		driver.findElement(By.xpath("(//button[@class='single_add_to_cart_button button alt'])")).click();
-        }
+}
 	
 	@Then("the user adds farmer brothers premium pomegranate herbal tea to the cart")
 	
 	public void the_user_adds_farmer_brothers_premium_pomegranate_herbal_tea_to_the_cart() throws InterruptedException {
 
-	Thread.sleep(2000);
+	   Thread.sleep(2000);
 		
 		driver.findElement(By.xpath("//a[@class='xoo-wsc-ft-btn xoo-wsc-btn xoo-wsc-ft-btn-cart']")).click();
 		
-        Thread.sleep(2000);
+       Thread.sleep(2000);
 		
 	    driver.findElement(By.xpath("//a[@class='checkout-button button alt wc-forward']")).click();
 		
-	Thread.sleep(2000);
+		Thread.sleep(2000);
 		
 	}
 
@@ -78,75 +84,92 @@ public class End2End {
     
 	 public void the_final_check_out_is_done_with_order_confirmation() throws InterruptedException {
 		
-                driver.findElement(By.name("billing_phone")).sendKeys("1234567890");	 
+       driver.findElement(By.name("billing_phone")).sendKeys("1234567890");	 
 		 
-         Thread.sleep(2000);
+       Thread.sleep(2000);
        
-	         driver.findElement(By.xpath("//div[@class='nxtstp stpbutn']")).click();
+	   driver.findElement(By.xpath("//div[@class='nxtstp stpbutn']")).click();
 	
-	 Thread.sleep(2000);
+	   Thread.sleep(2000);
 	   
-	         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-                 WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='nextstp-2 stpbutn']")));
-                 ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-                 ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
-
-	         WebElement element1 = driver.findElement(By.xpath("//div[@class='nextstp-3 stpbutn']"));
-	         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element1);
-	         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element1);
+	   WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+       WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='nextstp-2 stpbutn']")));
+       ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+       ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+  
+       WebElement element1 = driver.findElement(By.xpath("//div[@class='nextstp-3 stpbutn']"));
+	    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element1);
+	    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element1);
        
-	 Thread.sleep(2000);
+	    Thread.sleep(2000);
+	    
+	    WebElement element2 = driver.findElement(By.id("custom_shipping_method_ups_ground"));
+	    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element2);
+	    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element2);
 	   
-	         WebElement element2 = driver.findElement(By.id("payment_method_stripe"));
-	         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element2);
-	         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element2);
+	    Thread.sleep(2000);
+	    
+	    WebElement element3 = driver.findElement(By.id("payment_method_stripe"));
+	    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element3);
+	    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element3);
 	   
-	 try {
+	    try {
 	    	
-	    	 WebDriverWait wait1 = new WebDriverWait(driver,Duration.ofSeconds(10)); 
+	    	WebDriverWait wait1 = new WebDriverWait(driver,Duration.ofSeconds(10)); 
 
-	 Thread.sleep(2000);
+	    	Thread.sleep(2000);
 	    	
-	    	 WebElement iframeElement1 = driver.findElement(By.xpath("(//iframe[contains(@name,'__privateStripeFrame')])[1]"));
-	    	 driver.switchTo().frame(iframeElement1);
+	    	WebElement iframeElement1 = driver.findElement(By.xpath("(//iframe[contains(@name,'__privateStripeFrame')])[1]"));
+	    	driver.switchTo().frame(iframeElement1);
 
-                 WebElement cardNumberField = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@aria-label='Credit or debit card number']")));
-                 cardNumberField.sendKeys("4242424242424242");
+            WebElement cardNumberField = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@aria-label='Credit or debit card number']")));
+            cardNumberField.sendKeys("4242424242424242");
 
-                 driver.switchTo().defaultContent();
+            driver.switchTo().defaultContent();
 	        
-         Thread.sleep(2000);
+            Thread.sleep(2000);
             
-                 WebElement iframeElement2 = driver.findElement(By.xpath("(//iframe[contains(@name,'__privateStripeFrame')])[2]"));
-	    	 driver.switchTo().frame(iframeElement2);
+            WebElement iframeElement2 = driver.findElement(By.xpath("(//iframe[contains(@name,'__privateStripeFrame')])[2]"));
+	    	driver.switchTo().frame(iframeElement2);
 
-	    	 WebElement expDateField = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@aria-label='Credit or debit card expiration date']")));
-	    	 expDateField.sendKeys("1234");
+	    	WebElement expDateField = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@aria-label='Credit or debit card expiration date']")));
+	    	expDateField.sendKeys("1234");
 	    	
-	    	 driver.switchTo().defaultContent();
+	    	driver.switchTo().defaultContent();
 	    	
-         Thread.sleep(2000);
+            Thread.sleep(2000);
             
-                 WebElement iframeElement3 = driver.findElement(By.xpath("(//iframe[contains(@name,'__privateStripeFrame')])[3]"));
-	    	 driver.switchTo().frame(iframeElement3);
+            WebElement iframeElement3 = driver.findElement(By.xpath("(//iframe[contains(@name,'__privateStripeFrame')])[3]"));
+	    	driver.switchTo().frame(iframeElement3);
 
-	    	 WebElement cvcField = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@aria-label='Credit or debit card CVC/CVV']")));
-	    	 cvcField.sendKeys("567");
+	    	WebElement cvcField = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@aria-label='Credit or debit card CVC/CVV']")));
+	    	cvcField.sendKeys("567");
 	    	
-	    	 driver.switchTo().defaultContent();
+	    	driver.switchTo().defaultContent();
 	    
-	 } catch (Exception e) {
+	    } catch (Exception e) {
 	        e.printStackTrace();
-	 } finally {
+	    } finally {
 	    
-	 Thread.sleep(2000);
+	    	Thread.sleep(2000);
 			
-		 driver.findElement(By.xpath("//button[@name='woocommerce_checkout_place_order']")).click();
+		    driver.findElement(By.xpath("//button[@name='woocommerce_checkout_place_order']")).click();
 	    	
-	 Thread.sleep(15000);
+	        Thread.sleep(20000);
 	 
-	         driver.close();
+	        WebDriverWait wait2 = new WebDriverWait(driver,Duration.ofSeconds(10));
+	        
+	        WebElement orderReceived = wait2.until(ExpectedConditions.elementToBeClickable(By.xpath("//p[@class='woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received']")));
+	        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", orderReceived);
+	        
+	    	assertTrue(orderReceived.isDisplayed());
+	        
+	    	assertEquals("Thank you. Your order has been received.", orderReceived.getText());
+
+	    	Thread.sleep(2000);
+	        
+	      driver.close();
 	  
 	    }
-          }
-        }
+	 }
+ }
